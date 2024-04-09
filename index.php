98a76a030ef9bb09c29ae449b89a78ddff6e8aa4
dvidaalg <dvidal17gmail.com>
2024-04-09 11:03:10 -0400

    configurando cors index.php

--- index.php
+++ index.php
@@ -1,26 +1,29 @@
 <?php
-    require_once("config/conexion.php");
-    if(isset($_POST["enviar"]) and $_POST["enviar"]=="si"){
-        require_once("models/Usuario.php");
-        $usuario = new Usuario();
-        $usuario->login();
-    }
+// Habilitar CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Requested-With");
+
+require_once("config/conexion.php");
+if(isset($_POST["enviar"]) and $_POST["enviar"]=="si"){
+    require_once("models/Usuario.php");
+    $usuario = new Usuario();
+    $usuario->login();
+}
 ?>
 <!DOCTYPE html>
 <html>
 <head lang="es">
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<title>DiegoVidal</>::Acceso</title>
-
-	<link href="img/favicon.144x144.png" rel="apple-touch-icon" type="image/png" sizes="144x144">
-	<link href="img/favicon.114x114.png" rel="apple-touch-icon" type="image/png" sizes="114x114">
-	<link href="img/favicon.72x72.png" rel="apple-touch-icon" type="image/png" sizes="72x72">
-	<link href="img/favicon.57x57.png" rel="apple-touch-icon" type="image/png">
-	<link href="img/favicon.png" rel="icon" type="image/png">
-	<link href="img/favicon.ico" rel="shortcut icon">
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>DiegoVidal</title>
+    <link href="img/favicon.144x144.png" rel="apple-touch-icon" type="image/png" sizes="144x144">
+    <link href="img/favicon.114x114.png" rel="apple-touch-icon" type="image/png" sizes="114x114">
+    <link href="img/favicon.72x72.png" rel="apple-touch-icon" type="image/png" sizes="72x72">
+    <link href="img/favicon.57x57.png" rel="apple-touch-icon" type="image/png">
+    <link href="img/favicon.png" rel="icon" type="image/png">
+    <link href="img/favicon.ico" rel="shortcut icon">
     <link rel="stylesheet" href="public/css/separate/pages/login.min.css">
     <link rel="stylesheet" href="public/css/lib/font-awesome/font-awesome.min.css">
     <link rel="stylesheet" href="public/css/lib/bootstrap/bootstrap.min.css">
@@ -30,16 +33,11 @@
     <div class="page-center">
         <div class="page-center-in">
             <div class="container-fluid">
-
                 <form class="sign-box" action="" method="post" id="login_form">
-
-                    <!-- <input type="hidden" id="rol_id" name="rol_id" value="1"> -->
-
                     <div class="sign-avatar">
                         <img src="public/1.jpg" alt="" id="imgtipo">
                     </div>
                     <header class="sign-title" id="lbltitulo">Acceso Usuario</header>
-
                     <?php
                         if (isset($_GET["m"])){
                             switch($_GET["m"]){
@@ -54,7 +52,6 @@
                                         </div>
                                     <?php
                                 break;
-
                                 case "2";
                                     ?>
                                         <div class="alert alert-warning alert-icon alert-close alert-dismissible fade in" role="alert">
@@ -69,7 +66,6 @@
                             }
                         }
                     ?>
-
                     <div class="form-group">
                         <input type="text" id="usu_correo" name="usu_correo" class="form-control" placeholder="E-Mail"/>
                     </div>
@@ -90,7 +86,6 @@
             </div>
         </div>
     </div>
-
 <script src="public/js/lib/jquery/jquery.min.js"></script>
 <script src="public/js/lib/tether/tether.min.js"></script>
 <script src="public/js/lib/bootstrap/bootstrap.min.js"></script>
@@ -101,7 +96,6 @@
         $('.page-center').matchHeight({
             target: $('html')
         });
-
         $(window).resize(function(){
             setTimeout(function(){
                 $('.page-center').matchHeight({ remove: true });
@@ -113,8 +107,6 @@
     });
 </script>
 <script src="public/js/app.js"></script>
-
 <script type="text/javascript" src="datos.js"></script>
-
 </body>
 </html>
